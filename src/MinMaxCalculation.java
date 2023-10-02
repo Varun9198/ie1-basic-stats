648e7079787b2976740238d0068938fa9552d548
Pratyush Dubey <pratyushdubey29@github.com>
2023-10-01 21:05:58 -0400

    Added MinMaxCalculator class in src directory.

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxCalculation {
+
+    public Integer getMin(List<Integer> numbers) {
+        return Collections.min(numbers);
+    }
+
+    public Integer getMax(List<Integer> numbers) {
+        return Collections.max(numbers);
+    }
+}
